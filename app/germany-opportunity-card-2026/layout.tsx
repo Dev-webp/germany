5ad60ea7360b908bca1a96ee18cdccd8f89a4456
Dev-webp <vjcbangalore@gmail.com>
2026-03-03 17:47:50 +0530

    Initial commit

--- app/germany-opportunity-card-2026/layout.tsx
+++ app/germany-opportunity-card-2026/layout.tsx
@@ -34,27 +34,7 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* ✅ Previous Google Ads Global Site Tag */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-16767451796"
-          strategy="afterInteractive"
-        />
-        <Script id="ga4-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JPH0CGYTXT');
-          `}
-        </Script>
-        <Script id="google-ads-old" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-16767451796');
-          `}
-        </Script>
+       
 
         {/* ✅ Facebook Pixel */}
         <Script id="facebook-pixel" strategy="afterInteractive">
